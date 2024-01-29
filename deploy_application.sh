50335f44626a803470bc5abb3176fa3c93c7c04d
Karine <karine.v@yoocollab.com>
2024-01-29 17:21:56 +0400

    Some changes

--- deploy_application.sh
+++ deploy_application.sh
@@ -1,53 +1,55 @@
 #!/bin/bash
 
-aws_region="us-east-1"
-if [[ $? != 0 ]]
-  then
-    echo "Something wrong while getting aws default region, now exiting..."
-    exit 1
-fi
+AWS_DEFAULT_REGION="us-east-1"
 
-cd ./packer/
+# Change directory to packer/
+cd ./packer/ || exit 1
 
+# Build AMI using Packer
 packer build ubuntu-ami.json
-if [[ $? != 0 ]]
-  then
-    echo "Something wrong while executing packer build, now exiting..."
-    exit 1
+if [[ $? -ne 0 ]]; then
+  echo "Error: Packer build failed. Exiting..."
+  exit 1
 fi
 
-cd ../terraform/
+# Change directory back to the project root
+cd ..
 
+# Change directory to terraform/
+cd ../terraform/ || exit 1
+
+# Initialize Terraform
 terraform init
-if [[ $? != 0 ]]
-  then
-    echo "Something wrong while executing terraform init, now exiting..."
-    exit 1
+if [[ $? -ne 0 ]]; then
+  echo "Error: Terraform initialization failed. Exiting..."
+  exit 1
 fi
 
+# Display Terraform plan
 terraform plan
-if [[ $? != 0 ]]
-  then
-    echo "Something wrong while executing terraform plan, now exiting..."
-    exit 1
+if [[ $? -ne 0 ]]; then
+  echo "Error: Terraform plan generation failed. Exiting..."
+  exit 1
 fi
 
+# Apply Terraform changes
 terraform apply -auto-approve
-if [[ $? != 0 ]]
-  then
-    echo "Something wrong while executing terraform apply, now exiting..."
-    exit 1
+if [[ $? -ne 0 ]]; then
+  echo "Error: Terraform apply failed. Exiting..."
+  exit 1
 fi
 
+# Get the instance URL from Terraform output
 URL=$(terraform output instance_url)
 
-if [[ $? != 0 ]]
-  then
-    echo "Something wrong while getting game URL, now exiting..."
-    exit 1
+# Ensure the URL is retrieved successfully
+if [[ $? -ne 0 ]]; then
+  echo "Error: Unable to retrieve the instance URL. Exiting..."
+  exit 1
 fi
 
 # Remove double quotes from the URL
-URL=$(echo $URL | tr -d '"')
+URL=$(echo "$URL" | tr -d '"')
 
-echo "Instance URL is: http://${URL}"
+echo "Deployment completed successfully."
+echo "Instance URL: http://${URL}"
